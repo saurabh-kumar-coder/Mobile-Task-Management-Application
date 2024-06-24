cf87d795f99da1520d76e01ae34c1b271519c5c3
Saurabh Kumar <1355saurabh@gmail.com>
2024-06-24 16:24:54 +0530

    backend updates.

--- Backend/App.js
+++ Backend/App.js
@@ -17,21 +17,11 @@ app.use(express.json());
 
 app.use(morgan("dev"));
 
-//ROUTES
-// app.get("", (req, res) => {
-// 	res.status(200).json({
-// 		sucess: true,
-// 		message: "Welcome to our GET API",
-// 	});
-// });
-
-//routes
 app.use("/", require("./routes/taskRoute"));
 
 //PORT
 const PORT = process.env.PORT;
 
-// listen to the server
 app.listen(PORT, (req, res) => {
 	console.log(colors.bgGreen.white`Server is running on port ${PORT}`);
 });
